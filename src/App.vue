3c0f74be116e1310d3050d02122f38277581afbc
majazimnoch <majamzimnoch@gmail.com>
2024-01-26 10:34:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category 
+</script>
+
+<template>
+<div>
+  <h1>Hello world</h1>
+</div>
+</template>
+
