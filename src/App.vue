f90db3c45f08df89da928899d0318eb05c9024f3
majazimnoch <majamzimnoch@gmail.com>
2024-01-26 11:21:17 +0100

    two variables input cat and input cont

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,70 @@
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref, onMounted, computed, watch } from "vue";
 
-const todos = ref([])
-const name = ref('')
+const todos = ref([]);
+const name = ref("");
 
-const input_content = ref('')
-const input_category 
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+  todos.value.push({
+    content: input_content.value
+  })
+};
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+// remembering our name on localstorage
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
 </script>
 
 <template>
-<div>
-  <h1>Hello world</h1>
-</div>
-</template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+    <section class="create-todo">
+      <h3>Create a Todo</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your to do list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
 
+        <h4>Pick a category</h4>
+        <div class="options">
+
+          <label>
+            <input type="radio" name="category" id="category1" value="business" v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" id="category1" value="personal" v-model="input_category" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add a todo" />
+      </form>
+    </section>
+  </main>
+</template>
